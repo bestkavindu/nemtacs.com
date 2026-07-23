2ecf4951980269bd9c68eab0646b37d94da92358
bestkavindu@gmail.com <bestkavindu@gmail.com>
2026-07-23 23:55:11 +0530

    fix(logo): remove dynamic name binding for sidebar and brand components

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,13 +3,13 @@
 ])
 
 @if($sidebar)
-    <flux:sidebar.brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
+    <flux:sidebar.brand name="" {{ $attributes }}>
         <x-slot name="logo">
             <x-app-logo-icon class="h-8 rounded-md" />
         </x-slot>
     </flux:sidebar.brand>
 @else
-    <flux:brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
+    <flux:brand name="" {{ $attributes }}>
         <x-slot name="logo">
             <x-app-logo-icon class="h-8 rounded-md" />
         </x-slot>
